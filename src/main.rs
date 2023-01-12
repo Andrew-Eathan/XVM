945bedc1130be603fae5aef070abe21282de9bfe
Andrew-Eathan <42521608+Andrew-Eathan@users.noreply.github.com>
2023-01-12 12:41:17 +0200

    uhhhhhhj

--- src/main.rs
+++ src/main.rs
@@ -17,40 +17,57 @@ fn main() {
         io::stdout().flush().unwrap();
     });
 
+    // load byte 4 in register 0
     cpu.push_byte(Instruction::LOAD1B as u8);
     cpu.push_byte(0);
     cpu.push_byte(4);
 
+    // load byte 1 in register 1
     cpu.push_byte(Instruction::LOAD1B as u8);
     cpu.push_byte(1);
     cpu.push_byte(6);
 
+    // add them into register 2
     cpu.push_byte(Instruction::ADD as u8);
     cpu.push_byte(0);
     cpu.push_byte(1);
     cpu.push_byte(2);
     
+    // load 2 bytes
     cpu.push_byte(Instruction::LOAD2B as u8);
     cpu.push_byte(0);
     cpu.push_byte((500 & 0xff) as u8);
     cpu.push_byte(((500 & 0xff00) >> 8) as u8);
     
+    // multiply together
     cpu.push_byte(Instruction::MUL as u8);
     cpu.push_byte(0);
     cpu.push_byte(2);
     cpu.push_byte(1);
 
+    // load H byte
     cpu.push_byte(Instruction::LOAD1B as u8);
     cpu.push_byte(15);
     cpu.push_byte('H' as u8);
     
+    // call print interrupt
+    cpu.push_byte(Instruction::INT as u8);
+    cpu.push_byte(0);
+
+    // load i byte
+    cpu.push_byte(Instruction::LOAD1B as u8);
+    cpu.push_byte(15);
+    cpu.push_byte('i' as u8);
+    
+    // print
     cpu.push_byte(Instruction::INT as u8);
     cpu.push_byte(0);
 
     cpu.push_byte(Instruction::LOAD1B as u8);
     cpu.push_byte(15);
-    cpu.push_byte('i' as u8);
-    
+    cpu.push_byte('!' as u8);
+
+    // print
     cpu.push_byte(Instruction::INT as u8);
     cpu.push_byte(0);
 
